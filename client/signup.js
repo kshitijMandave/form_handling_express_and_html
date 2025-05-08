425f2a6f4234504a9ed1fca935f1b098d2ac7cba
kshitijMandave <kshitij.mandwe@gmail.com>
2025-05-08 19:25:10 +0530

    code added

--- client/signup.js
+++ client/signup.js
@@ -1,13 +1,41 @@
 function getUser() {
   const fullname = document.getElementById("fullname").value;
   const email = document.getElementById("email").value;
+  const phone = document.getElementById("phone").value;
   const gender = document.getElementById("gender").value;
   const password = document.getElementById("password").value;
   const confirmPassword = document.getElementById("confirmPassword").value;
 
   console.log("Full Name:", fullname);
   console.log("Email:", email);
+  console.log("phone", phone);
   console.log("Gender:", gender);
   console.log("Password:", password);
   console.log("Confirm Password:", confirmPassword);
+
+  let newuser = {
+    name: fullname,
+    email: email,
+    phone: phone,
+    gender: gender,
+    password: password,
+    confirmPassword: confirmPassword,
+  };
+
+  fetch("http://localhost:3065/user", {
+    method: "POST",
+    "Content-Type": "application/jaon",
+    body: JSON.stringify,
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      alert("user Ragisterd");
+      console.log(data);
+    })
+    .catch((error) => {
+      alert("Something went wrong");
+      console.log(error);
+    });
 }
--- server/app.js
+++ server/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 
-app.get("", () => {});
+app.post("/user", (req, res) => {}); //http://localhost:3065/user
+
 app.listen(3065, () => {
   console.log("Server Started");
 });
